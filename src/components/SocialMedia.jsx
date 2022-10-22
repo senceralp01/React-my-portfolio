242ffd38fb9b44fbfaadcaf55d2b17cb7a4a37dc
sencer2022 <senceralpdogdu@gmail.com>
2022-10-22 14:29:31 +0300

    social media icons and project descriptions have been fixed

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -5,17 +5,19 @@ const SocialMedia = () => {
   return (
     <div className='app__social'>
       <div>
-        <a href="https://www.linkedin.com/in/senceralp" rel="noreferrer" target="_blank" style={{ marginTop: "5px" }}>
+        <a href="https://www.linkedin.com/in/senceralp" rel="noreferrer" target="_blank">
           <BsLinkedin />
         </a>
       </div>
       <div>
-        <a href="https://github.com/senceralp01" rel="noreferrer" target="_blank" style={{ marginTop: "5px" }}>
+        <a href="https://github.com/senceralp01" rel="noreferrer" target="_blank">
           <BsGithub />
         </a>
       </div>
       <div>
-        <BsInstagram />
+        <a href="https://senceralp.netlify.app/" rel="noreferrer" target="_blank">
+          <BsInstagram />
+        </a>
       </div>
     </div>
   )
