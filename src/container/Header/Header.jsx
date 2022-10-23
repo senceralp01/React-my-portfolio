02b703fe984dd5490361cf273453aa04685bcb15
sencer2022 <senceralpdogdu@gmail.com>
2022-10-23 22:58:03 +0300

    header

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -27,7 +27,7 @@ const Header = () => {
       >
         <div className="app__header-badge">
           <div className="badge-cmp app__flex">
-            <span>👋</span>
+            <span>🤝</span>
             <div style={{ marginLeft: 20 }}>
               <p className="p-text">Hello, I am</p>
               <h1 className="head-text">sencer alp</h1>
