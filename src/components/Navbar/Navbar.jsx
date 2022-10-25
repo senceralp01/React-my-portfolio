ee19b6ffa816b02c37f93f21bb4928bff22bbd2c
sencer2022 <senceralpdogdu@gmail.com>
2022-10-26 02:14:43 +0300

    Some changes and additions have been being continued to implement on this section.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => {
           >
             <HiX onClick={() => setToggle(false)} />
             <ul>
-              {['home', 'about', 'projects', 'skills', 'testimonials', 'contact'].map((item) => (
+              {['greeting', 'about', 'projects', 'skills', 'testimonials', 'contact'].map((item) => (
                 <li key={item}>
                   <a href={`#${item}`} onClick={() => setToggle(false)}>{item}</a>
                 </li>
--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -3,19 +3,31 @@ import { motion } from 'framer-motion';
 
 import { AppWrap, MotionWrap } from '../../wrapper';
 import './About.scss';
-import { urlFor, client} from '../../client';
+import { urlFor, client } from '../../client';
 
 
 const About = () => {
   const [abouts, setAbouts] = useState([]);
+  const [educations, setEducations] = useState([]);
+  const [languages, setLanguages] = useState([]);
 
   useEffect(() => {
     const query = '*[_type == "abouts"]';
+    const educationQuery = '*[_type == "education"]';
+    const languageQuery = '*[_type == "language"]';
 
     client.fetch(query)
-      .then((data) => setAbouts(data))
+      .then((data) => setAbouts(data));
+    client.fetch(educationQuery)
+      .then((data) => {
+        setEducations(data[0].education)
+      });
+    client.fetch(languageQuery)
+      .then((data) => {
+        setLanguages(data[0].language)
+      });
   }, []);
-  
+
 
   return (
     <>
@@ -33,10 +45,60 @@ const About = () => {
             <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
             <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
-            
+
           </motion.div>
         ))}
+      </div>
 
+      <div className='app__moreinfo'>
+        <div className='app__educations'>
+          <h1>Education</h1>
+          {educations.map((education, index) => (
+            <motion.div
+              className='app__education'
+              key={index}
+              whileInView={{ opacity: [0, 1] }}
+              transition={{ duration: 0.5, ease: 'easeInOut' }}
+            >
+              <div className='app__education-logo'>
+                <img src={urlFor(education.img)} alt="school-logo" />
+              </div>
+
+              <div className='app__education-info'>
+                <div className='info-school'>
+                  <h4>{education.department}</h4>
+                  <p>{education.school}</p>
+                </div>
+                <div className='info-year'>
+                  <p>{education.year}</p>
+                  <p>{education.city}</p>
+                </div>
+              </div>
+            </motion.div>
+          ))}
+        </div>
+        <div className='app__languages'>
+          <h1>Languages</h1>
+          <div className='app__languages-cover'>
+            {languages.map((lang, index) => (
+              <motion.div
+                className='app__language'
+                key={index}
+                whileInView={{ opacity: [0, 1] }}
+                transition={{ duration: 0.5, ease: 'easeInOut' }}
+              >
+
+                <div className='app__language-info'>
+                  <h4>{lang.language}</h4>
+                  <p>{lang.level}</p>
+                </div>
+                <div className='app__language-flag'>
+                  <img src={urlFor(lang.img)} alt="language-flag" />
+                </div>
+              </motion.div>
+            ))}
+          </div>
+        </div>
       </div>
     </>
   )
